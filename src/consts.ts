2b126a4e50831013b7a1fe503b3d5cf4edf35765
towardsuffering <christopher.rotunno@outlook.com>
2024-01-29 00:02:45 -0600

    open graph image

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Chris Blogs';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'A personal platform for development, discovery, and content creation';
 export const OG_IMAGE = '/chrisblogsog.png'; // Path relative to the public directory
