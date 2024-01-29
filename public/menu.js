97faf3bffb42f8e3f9c2fc1048d5fec1a3667354
towardsuffering <christopher.rotunno@outlook.com>
2024-01-29 12:45:55 -0600

    dynamic display

---
+++ public/menu.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.hamburger').addEventListener('click', () => {
+      document.querySelector('.nav-links').classList.toggle('expanded');
+  });
+});
